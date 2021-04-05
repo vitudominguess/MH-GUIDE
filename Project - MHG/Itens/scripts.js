fc6e6bbaf1df7c6088e7d421a7b23a1850c9a8a0
Vitor Pessoa <67473131+RUIVS@users.noreply.github.com>
2021-04-05 16:29:20 -0300

    ajustes do rodapé

--- Project - MHG/Itens/scripts.js
+++ Project - MHG/Itens/scripts.js
@@ -40,13 +40,36 @@ var numero  = prompt('Insira o número aqui');
 numeroFormatado = parseInt(numero);
 alert(numeroFormatado * 0.5);
 */
- $(document).ready(function() {
-	$('#listar_Item').dataTable( {
-		"language": {
+      $(document).ready(function() {
+        $('#listar_item').DataTable({
+          "language": {
+          "search": "Buscar:",
 		  "paginate": {
-			"previous": "Anterior",
-			"next": "Próximo"
-		  }
+          	  "first": "Inicio",
+              "last": "Fim",
+			  "previous": "Anterior",
+			  "next": "Próximo"
+		  },
+      "aria": {
+        "sortAscending":  ": ative para classificar a coluna em ordem crescente",
+        "sortDescending": ": ative para classificar a coluna em ordem decrescente"
+      },
+      "lengthMenu": '<select>'+
+        '<option value="10">10</option>'+
+        '<option value="20">20</option>'+
+        '<option value="30">30</option>'+
+        '<option value="40">40</option>'+
+        '<option value="50">50</option>'+
+        '<option value="-1">All</option>'+
+        '</select> Registros',
+      "info": "Mostrando Página _PAGE_ de _PAGES_",
+      "infoEmpty":      "Mostrando 0 de _PAGES_ Registros",
+      "infoFiltered":   "(filtrado de _MAX_ registros totais)",
+      "decimal":        "",
+      "emptyTable":     "Sem dados disponíveis na tabela",
+      "zeroRecords":    "Nenhum registro correspondente encontrado",
+      "loadingRecords": "Carregando...",
+      "processing":     "Processando..."
 		}
-	  } );
-		});
+        });
+      } );
